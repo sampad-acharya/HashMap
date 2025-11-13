fbb59cf55339cd087cbc2a501678547d3fca94e1
Sampad Acharya <sampadacharya@Sampads-MacBook-Pro.local>
2025-11-12 21:24:50 -0500

    Adding dynamic memory implementation

---
+++ Using Dynamic Memory/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <functional>
+#include "HashMap.hpp"
+
+
+int main() {
+    HashMap<std::string, int> map{10}; 
+    map.insert("apple", 10);
+    map.insert("banana", 20);
+    map.insert("orange", 30); 
+    int val;
+    if (map.get("orange", val)) {
+        std::cout << "orange: " << val << "\n";
+    }
+
+    std::cout << "Current size: "<< map.count() << "\n";
+    map.erase("apple"); 
+    std::cout << "Current size: "<< map.count() << "\n";
+    map["mango"] = 10;
+    std::cout << "mango: "<< map["fruit_new"] << "\n";
+
+};
